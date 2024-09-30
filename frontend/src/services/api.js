73fa79da1b61b2e26bf1655448f93090cb546ff1
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-09-30 21:08:21 +0700

    add url backend on frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "https://catatan-kuliah-api.vercel.app";
+const API_URL = "https://catatan-kuliah-api.vercel.app" || "http://localhost:3000";
 
 const api = axios.create({
   baseURL: API_URL,
